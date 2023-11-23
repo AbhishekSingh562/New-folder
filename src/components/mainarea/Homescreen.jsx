77f8dbbb98ec8f6558907c88878f711439be5a72
abhishek <abhisingh562002offc@gmail.com>
2023-11-23 15:20:33 +0530

    changed the msg

--- src/components/mainarea/Homescreen.jsx
+++ src/components/mainarea/Homescreen.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from "./homescreen.module.css";
-import img from '../../assets/one.png'
+import img from "../../assets/one.png";
 
 function Homescreen() {
   return (
     <div className={styles.div}>
-      <img src={img} alt=""  className={styles.gap}/>
+      <img src={img} alt="" className={styles.gap} />
       <h1 className={styles.gap}>Pocket Notes</h1>
       <p className={styles.gap}>
-        Send and receive messages without keeping your phone online. Use Pocket
-        Notes on up to 4 linked devices and 1 mobile phone
+        Hey and welcome all in Abhishek's note application, where you can save
+        your thoughts and note by giving it a name.
       </p>
     </div>
   );
